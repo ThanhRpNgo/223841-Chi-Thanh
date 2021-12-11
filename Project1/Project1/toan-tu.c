48f702a56f8699340d0cd65ca94ffb8af01909b1
ThanhRpNgo <88963198+ThanhRpNgo@users.noreply.github.com>
2021-12-11 20:51:24 +0700

    bai toan tu if else

--- Project1/Project1/toan-tu.c
+++ Project1/Project1/toan-tu.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
-
 void main()
 {
-	unsigned short x = 0;
-	unsigned short y = 0;
-	unsigned short high = 0xabcd;
-	unsigned short low = 0xabcd;
-	x = high >> 8;
-	y = low << 8;
-	printf("value: %x\r\n", x);
-	printf("value: %x\r\n", y);
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	printf("nhap cho du lieu a: ");
+	scanf_s("%d", &a);
+	printf("nhap cho du lieu b: ");
+	scanf_s("%d", &b);
+	printf("nhap cho du lieu c: ");
+	scanf_s("%d", &c);
+	int delta = b * b - 4*a*c;
+	if (delta < 0)
+	{
+		printf("phuong trinh vo nghiem");
+
+	}
+	else if (delta > 0)
+	{
+		printf("phuong trinh co 2 nghiem phan biet");
+	}
+	
+	else
+	{
+		printf("phuong trinh co 1 nghiem chung");
+	}
 
 }
